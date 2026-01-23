b84edf9d2d9b4b219ab4029e14f6b9204f18dd7e
Gustavo Figueiredo <gustavo_luiz97@hotmail.com>
2026-01-23 10:28:20 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,20 +1,19 @@
 import { test, expect } from '@playwright/test'
 
+// AAA - Arrange, Act, Assert
+
 test('Deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/')
-  
-  // Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-A204IM')
-
   await page.getByTestId('search-order-button').click()
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-A204IM')
 
