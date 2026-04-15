07a137e5909160ff1059289d33595a9dd2182e22
Gustavo Figueiredo <gustavo_luiz97@hotmail.com>
2026-04-15 14:03:07 -0300

    Atividades até aula 13 do módulo 3

--- playwright/support/fixtures.ts
+++ playwright/support/fixtures.ts
@@ -1,19 +1,21 @@
-import { test as base, expect } from '@playwright/test'
+import { test as base } from '@playwright/test'
+import { createConfiguratorActions } from './actions/configuratorActions'
 import { createOrderLookupActions } from './actions/orderLookupActions'
 
 type App = {
   orderLookup: ReturnType<typeof createOrderLookupActions>
+  configurator: ReturnType<typeof createConfiguratorActions>
 }
 
 export const test = base.extend<{ app: App }>({
   app: async ({ page }, use) => {
     const app: App = {
       orderLookup: createOrderLookupActions(page),
+      configurator: createConfiguratorActions(page),
     }
 
     await use(app)
   },
 })
 
-export { expect }
-
+export { expect } from '@playwright/test'
